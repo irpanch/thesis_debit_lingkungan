2cccc876616946fd73dea0c96115788da93050e1
irpanch <irpanch@gmail.com>
2023-07-03 16:58:07 +0700

    adding code to sapan and nanjung discharge station

--- 2_code/tidy_data.R
+++ 2_code/tidy_data.R
@@ -8,29 +8,54 @@ library(fasstr)
 # import and checking -----------------------------------------------------
 
 dayeuh_kolot <- read_csv("1_data/hymos_dayeuh_kolot.csv")
+sapan <- read_csv("1_data/hymos_sapan.csv")
+nanjung <- read_csv("1_data/hymos_nanjung.csv",
+                    col_types = cols(value = col_character(), 
+                                     station = col_character()))
 
 ## cek data hasil import
 glimpse(dayeuh_kolot)
+glimpse(sapan)
+glimpse(nanjung)
 
 # filter, tidy, fix data type ---------------------------------------------
 
 ## ambil kolom yang penting, filter terhadap data yang ada. rubah format.
 dayeuh_kolot_filter <- dayeuh_kolot %>% select(date:station) %>% 
   filter(value != "-")
+sapan_filter <- sapan %>% select(date:station) %>% 
+  filter(value != "-")
+nanjung_filter <- nanjung %>% select(date:station) %>% 
+  filter(value != "-")
 
 ## convert kolom debit menjadi numeric
 dayeuh_kolot_filter$value <- as.numeric(dayeuh_kolot_filter$value)
+sapan_filter$value <- as.numeric(sapan_filter$value)
+nanjung_filter$value <- as.numeric(nanjung_filter$value)
+nanjung_filter$station <- as.character(nanjung_filter$station)
 
 ## convert kolom tanggal menjadi format apa adanya di excel
 dayeuh_kolot_filter$date <- strptime(dayeuh_kolot_filter$date,format="%d-%b-%y")
+sapan_filter$date <- strptime(sapan_filter$date,format="%d-%b-%y")
+nanjung_filter$date <- strptime(nanjung_filter$date,format="%d-%b-%y")
+
 ## convert kolom tanggal menjadi format seperti yang diinginkan
 dayeuh_kolot_filter$date <- as.Date(dayeuh_kolot_filter$date,"%Y-%m-%d")
+sapan_filter$date <- as.Date(sapan_filter$date,"%Y-%m-%d")
+nanjung_filter$date <- as.Date(nanjung_filter$date,"%Y-%m-%d")
 
 ## cek data hasil import
 glimpse(dayeuh_kolot_filter)
+glimpse(sapan_filter)
+glimpse(nanjung_filter)
 
 # plotting ----------------------------------------------------------------
 ggplot(data=dayeuh_kolot_filter, aes(x=date, y=value))+geom_line()
+
+ggplot() +
+  geom_line(data=dayeuh_kolot_filter, aes(x=date, y=value),color='green')+
+  geom_line(data=sapan_filter, aes(x=date, y=value),color='blue')+
+  geom_line(data=nanjung_filter, aes(x=date, y=value),color='orange')
 
 # analisa_debit -----------------------------------------------------------
 ## rename column to adjust the fasstr package
